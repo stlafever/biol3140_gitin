ae5c1b018b6061cea50afb075d765f8d1fe06025
Steven LaFever <lafever@bc.edu>
2021-09-26 14:54:53 -0400

    updates, think it's done

--- Module3S.R
+++ Module3S.R
@@ -51,10 +51,10 @@ anole.log <- anole.log %>%
 #Q3: Plot residuals of linear models against covariates
 #plot perch height residuals vs Ecomorphs
 anole.log%>%
-  ggplot(aes(x=PH,y=res.ph)) +geom_point()#+geom_smooth(method="lm")
+  ggplot(aes(x=PH,y=res.ph)) +geom_point()+geom_smooth(method="lm")
 #plot perch diameter residuals vs Ecomorphs
 anole.log%>%
-  ggplot(aes(x=ArbPD,y=res.pd)) + geom_point()#+geom_smooth(method="lm")
+  ggplot(aes(x=ArbPD,y=res.pd)) + geom_point()+geom_smooth(method="lm")
 
 #Q4: Construct phylogenetic least squares models of the relationships
 #PGLS under BM, w perchh height
@@ -68,7 +68,7 @@ pgls.BM3 <- gls(HTotal~SVL+PH+ArbPD, correlation = corBrownian(0,phy = anole.tre
 anole.aic <- AICc(pgls.BM0,pgls.BM1, pgls.BM2, pgls.BM3) %>% print()
 anole.aicw <- aicw(anole.aic$AICc)
 print(anole.aicw)
-#perch diameter is a significant predictor of hindlimb length (delta AIC ~ 10)
+#perch diameter is a significant predictor of hindlimb length (delta AIC ~ 10) but perch height is not
 
 #Add phylogenetic residuals to anole.log
 anole.log <- anole.log %>% mutate(phylo.res1=residuals(pgls.BM1))
@@ -77,8 +77,11 @@ anole.log <- anole.log %>% mutate(phylo.res3=residuals(pgls.BM3))
 anole.log %>% head()
 
 #Q6: Plot effect of perch diameter of effect perch diameter on hindlimb residuals
+#I used the residual of the model that just took into account perch diameter because
+#while the model that took both into account was slightly better, the difference was small
+#and it used more parameters
 anole.log%>%
   dplyr::select(ArbPD,res.pd,phylo.res2)%>%
   pivot_longer(cols=c("res.pd","phylo.res2"))%>%
   print%>%
-  ggplot(aes(x=ArbPD,y=value)) +geom_point() +stat_summary(fun=mean, geom="point", size=3)+facet_grid(name~.,scales = "free_y")+ylab("residual")
+  ggplot(aes(x=ArbPD,y=value)) +geom_point() +stat_summary(fun=mean, geom="point", size=3)+facet_grid(name~.,scales = "free_y")+ylab("residual")+geom_smooth(method="lm")
